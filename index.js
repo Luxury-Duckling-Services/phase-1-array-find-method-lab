1ee813cc635e570770b15e4fa546403ad70acb88
Yifan <datasciencewyfan@gmail.com>
2022-05-17 19:29:49 -0700

    commit tree 1be62b6d30b11193fc42df404781f52e16f4ab1a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+function isOdd(object) {
+    return (object.result === "W");
+}
+
+let inp = [
+    { year: "2011", result: "S"},
+    { year: "2012", result: "W"},
+    { year: "2013", result: "T"},
+]
+
+let outp = inp.find(isOdd)
+
+console.log(outp)
+console.log(outp.year)
+
+
+const superbowlWin = (records) => {
+    let winRecord = records.find( record => record.result === "W" )
+    if (winRecord === undefined) {
+        return undefined
+    } else {
+        return winRecord.year
+    }
+}
